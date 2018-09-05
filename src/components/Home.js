3529da6bd61d81d75e2a43d02ad8afb61d157939
Bella Dear <arabelladear@hotmail.co.uk>
2018-09-05 23:38:40 +0100

    first commit

---
+++ src/components/Home.js
@@ -0,0 +1,18 @@
+import React from 'react';
+// import axios from 'axios';
+
+class Home extends React.Component {
+
+  state = {};
+
+  render() {
+    return (
+      <main>
+        <h1>Bonjour Home</h1>
+      </main>
+    );
+  }
+
+}
+
+export default Home;
---
+++ src/components/Show.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+class Show extends React.Component {
+
+  state = {
+  };
+
+  render() {
+    return (
+      <main>
+        <h1>Bonjour Show</h1>
+      </main>
+    );
+  }
+}
+
+export default Show;
