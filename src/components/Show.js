efc1c8f84660aa23f08db7d7a2127858c2d2fa1f
Bella Dear <arabelladear@hotmail.co.uk>
2018-09-06 23:43:50 +0100

    fixed JSON problem and restricted from favouriting twice

--- src/components/Show.js
+++ src/components/Show.js
@@ -3,7 +3,9 @@ import axios from 'axios';
 
 class Show extends React.Component {
 
-  state = {};
+  state = {
+    favourite: false
+  };
 
   componentWillMount = () => {
     axios({
@@ -13,16 +15,23 @@ class Show extends React.Component {
         Authorization: 'Client-ID 3303fbd52fa053ed62beecf82a93c6da73f69c79fa7ee9daf0d71b0acdba6d7f'
       }
     })
-      .then(res => this.setState({ photo: res.data }));
+      .then(res => this.setState({ photo: res.data }))
+      .then(() => {
+        const favourites = JSON.parse(localStorage.getItem('favourites').split());
+        if(favourites.includes(this.state.photo.urls.regular)) this.setState({ favourite: true });
+      });
   }
 
   handleFavourite = () => {
-    document.getElementById('star').classList.toggle('fas');
-    if(localStorage.getItem('favourites')) {
-      const storedArray = JSON.parse(localStorage.getItem('favourites').split());
-      const newPhotoArray = storedArray.concat(this.state.photo.urls.regular.split());
-      localStorage.setItem('favourites', JSON.stringify(newPhotoArray));
-    } else localStorage.setItem('favourites', JSON.stringify(this.state.photo.urls.regular.split()));
+    if(!this.state.favourite) {
+      this.setState({ favourite: true });
+      document.getElementById('star').classList.toggle('fas');
+      if(localStorage.getItem('favourites')) {
+        const storedArray = JSON.parse(localStorage.getItem('favourites').split());
+        const newPhotoArray = storedArray.concat(this.state.photo.urls.regular.split());
+        localStorage.setItem('favourites', JSON.stringify(newPhotoArray));
+      } else localStorage.setItem('favourites', JSON.stringify(this.state.photo.urls.regular.split()));
+    }
   }
 
   render() {
@@ -38,7 +47,7 @@ class Show extends React.Component {
             <hr />
             {this.state.photo && <h2 className="title is-5">{this.state.photo.views ? `${this.state.photo.views} Views` : '0 Views'}</h2>}
             {this.state.photo && <h2 className="title is-5">{this.state.photo.likes ? `${this.state.photo.likes} Likes` : '0 Likes'}</h2>}
-            <h2><i id="star" onClick={this.handleFavourite} className="far fa-star"></i> Add to Favourites</h2>
+            <h2><i id="star" onClick={this.handleFavourite} className={`${!this.state.favourite ? 'far' : 'fas'} fa-star`}></i> Add to Favourites</h2>
           </div>
         </div>
       </main>
