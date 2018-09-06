fece8fe4c3e41c5aa1579028f7846b2aa196d731
Bella Dear <arabelladear@hotmail.co.uk>
2018-09-06 22:04:03 +0100

    added style on homepage and added hover feature

--- src/components/Home.js
+++ src/components/Home.js
@@ -14,17 +14,13 @@ class Home extends React.Component {
 
   getPictures = () => {
     axios({
-      url: 'https://api.unsplash.com/photos/',
+      url: 'https://api.unsplash.com/photos/random?count=30',
       method: 'GET',
       headers: {
         Authorization: 'Client-ID 3303fbd52fa053ed62beecf82a93c6da73f69c79fa7ee9daf0d71b0acdba6d7f'
       }
     })
       .then(res => this.setState({ photos: res.data }));
-  }
-
-  reloadPage = () => {
-    location.reload();
   }
 
   render() {
@@ -34,9 +30,10 @@ class Home extends React.Component {
         <button className="button" onClick={this.getPictures}>MIX</button>
         <div className="columns is-multiline">
           {this.state.photos && this.state.photos.map(photo =>
-            <div key={photo.id} className="column is-one-third">
+            <div key={photo.id} className="column is-one-third img-wrap">
               <Link  to={`/show/${photo.id}`}>
-                <img src={photo.urls.regular} className="home-image" />
+                <img src={photo.urls.regular} className="home-img" />
+                {photo.location && <p className="img-description">{photo.location.name}</p>}
               </Link>
             </div>
           )}
