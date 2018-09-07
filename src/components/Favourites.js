50c6636387eb57c92a701b2442abcf22656c7c92
Bella Dear <arabelladear@hotmail.co.uk>
2018-09-07 11:53:42 +0100

    added a clear favourites button

--- src/components/Favourites.js
+++ src/components/Favourites.js
@@ -8,8 +8,13 @@ class Favourites extends React.Component {
   };
 
   componentWillMount = () => {
-    const favourites = JSON.parse(localStorage.getItem('favourites').split());
+    const favourites = JSON.parse(localStorage.getItem('favourites'));
     this.setState({ photos: favourites });
+  }
+
+  clearAll = () => {
+    localStorage.removeItem('favourites');
+    location.reload();
   }
 
   render() {
@@ -17,6 +22,7 @@ class Favourites extends React.Component {
     return (
       <main>
         {!this.state.photos && <h1 className="title is-4 is-centered">No favourites yet!</h1>}
+        {this.state.photos && <button onClick={this.clearAll} className="button">Clear All</button>}
         <div className="columns is-multiline">
           {this.state.photos && this.state.photos.map(photo =>
             <div key={photo.id} className="column is-one-third img-wrap">
